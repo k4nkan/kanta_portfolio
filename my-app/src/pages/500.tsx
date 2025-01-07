d69593d64cb99fd646341c65a46fac5cc6643213
k4nkan <kanta.y.0126@gmail.com>
2025-01-07 14:06:46 +0900

    #14 カスタムエラーページの作成

---
+++ my-app/src/pages/500.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Custom404 = () => {
+  return (
+    <>
+      <h1>500 - server error</h1>
+      <p>申し訳ございません</p>
+      <p>現在このページは表示できません...</p>
+    </>
+  );
+};
+
+export default Custom404;
