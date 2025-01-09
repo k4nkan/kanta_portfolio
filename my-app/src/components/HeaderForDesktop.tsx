f289aafa006276362192ed0d3be7885bc505f014
k4nkan <kanta.y.0126@gmail.com>
2025-01-09 16:56:09 +0900

    #26 フォントの追加

--- my-app/src/components/HeaderForDesktop.tsx
+++ my-app/src/components/HeaderForDesktop.tsx
@@ -3,11 +3,11 @@ import React from "react";
 
 const HeaderForDesktop = () => {
   return (
-    <div className="flex justify-between items-center p-7 px-5 md:px-10 lg:px-15  bg-gray-100">
+    <div className="flex justify-between items-center p-7 px-5 pb-5 md:px-10 lg:px-15 border-b-4 border-black">
       <Link href={"/"}>
-        <div className="text-xl md:text-2xl lg-3xl">Kanta Yamauchi</div>
+        <div className="text-2xl md:text-3xl lg-4xl">Kanta Yamauchi</div>
       </Link>
-      <div className="flex gap-5 md:gap-7.5 lg:gap-10 text-sm md:text-lg lg-xl">
+      <div className="flex gap-5 md:gap-7.5 lg:gap-10 text-lg md:text-xl lg-2xl">
         <Link href={"/"}>
           <div>Top</div>
         </Link>
