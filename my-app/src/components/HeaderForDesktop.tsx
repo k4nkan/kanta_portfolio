3ef97ce9a606aa5e57f651b42aafa7e37bc8cfce
k4nkan <kanta.y.0126@gmail.com>
2025-01-10 21:42:03 +0900

    #25 framer-motionの追加

--- my-app/src/components/HeaderForDesktop.tsx
+++ my-app/src/components/HeaderForDesktop.tsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import Link from "next/link";
 import React from "react";
 
@@ -5,17 +6,17 @@ const HeaderForDesktop = () => {
   return (
     <div className="flex justify-between items-center p-7 px-5 pb-5 md:px-10 lg:px-15 border-b-4 border-black">
       <Link href={"/"}>
-        <div className="text-2xl md:text-3xl lg-4xl">Kanta Yamauchi</div>
+        <motion.div whileHover={{scale:1.1}} whileTap={{scale:0.9}} className="text-2xl md:text-3xl lg-4xl">Kanta Yamauchi</motion.div>
       </Link>
       <div className="flex gap-5 md:gap-7.5 lg:gap-10 text-lg md:text-xl lg-2xl">
         <Link href={"/"}>
-          <div>Top</div>
+          <motion.div whileHover={{scale:1.1}} whileTap={{scale:0.9}}>Top</motion.div>
         </Link>
         <Link href={"/gallery"}>
-          <div>Gallery</div>
+          <motion.div whileHover={{scale:1.1}} whileTap={{scale:0.9}}>Gallery</motion.div>
         </Link>
         <Link href={"/access"}>
-          <div>Access</div>
+          <motion.div whileHover={{scale:1.1}} whileTap={{scale:0.9}}>Access</motion.div>
         </Link>
       </div>
     </div>
