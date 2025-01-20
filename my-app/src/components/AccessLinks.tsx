5177c1953295f6433074dd227077031960c0794f
k4nkan <kanta.y.0126@gmail.com>
2025-01-20 11:41:07 +0900

    #40 アクセスページレスポンシブ対応の途中経過

--- my-app/src/components/AccessLinks.tsx
+++ my-app/src/components/AccessLinks.tsx
@@ -7,41 +7,16 @@ type AccessLinksProps = {
 };
 
 const AccessLinks: React.FC<AccessLinksProps> = ({ icon, url, title }) => {
-  const [isWide, setIsWide] = useState(false); // サイズ監視の状態
-  const linkRef = useRef<HTMLAnchorElement | null>(null);
-
-  useEffect(() => {
-    if (!linkRef.current) return;
-
-    const observer = new ResizeObserver((entries) => {
-      for (let entry of entries) {
-        const width = entry.contentRect.width;
-        setIsWide(width > 450); // 450pxを超えたらtrueに
-      }
-    });
-
-    observer.observe(linkRef.current);
-
-    return () => {
-      if (linkRef.current) {
-        observer.unobserve(linkRef.current);
-      }
-    };
-  }, []);
-
   return (
-    <div className={`my-3 mx-20 ${isWide ? "m-auto" : ""}`}>
-      <a
-        ref={linkRef}
-        href={url}
-        target="_blank"
-        rel="noopener noreferrer"
-        className="flex items-center justify-center gap-4 p-3 border-2 border-black text-lg md:text-xl lg:text-2xl rounded-xl w-full max-w-[450px]"
-      >
-        <span className="text-3xl md:text-4xl lg:text-5xl">{icon}</span>
-        <span>{title}</span>
-      </a>
-    </div>
+    <a
+      href={url}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="flex items-center justify-center gap-4 p-3 border-2 border-black text-lg rounded-xl w-full"
+    >
+      <span className="text-3xl">{icon}</span>
+      <span>{title}</span>
+    </a>
   );
 };
 
--- my-app/src/pages/access/index.tsx
+++ my-app/src/pages/access/index.tsx
@@ -6,33 +6,33 @@ import { FaTwitterSquare } from "react-icons/fa";
 
 const Home = () => {
   return (
-    <div className="flex flex-col justify-center h-screen mx-auto">
-      <div className="text-center text-3xl xxs:text-4xl xs:text-5xl sm:text-6xl md:text-7xl lg:text-8xl px-4 py-3">
-        Access Page
-      </div>
-      <div className="text-center text-[16px] md:text-lg lg:text-xl px-4 pb-7">
+    <div className="flex flex-col gap-4 justify-center items-center" style={{ minHeight: "calc(100vh - 100px)" }}>
+      <div className="text-center text-4xl">Access Page</div>
+      <div className="text-center text-xl">
         各種SNSへのアクセスページです。
       </div>
-      <AccessLinks
-        icon={<FaGithub />}
-        url="https://github.com/k4nkan"
-        title="Github"
-      />
-      <AccessLinks
-        icon={<FaFacebook />}
-        url="https://www.facebook.com/profile.php?id=61558039312920"
-        title="Facebook"
-      />
-      <AccessLinks
-        icon={<FaInstagramSquare />}
-        url="https://www.instagram.com/yamakan126?igsh=eDh6cWRuYmxxaWk4&utm_source=qr"
-        title="Instagram"
-      />
-      <AccessLinks
-        icon={<FaTwitterSquare />}
-        url="https://x.com/caaantaaa?s=21"
-        title="X (Twitter)"
-      />
+      <div className="flex flex-col gap-3 items-center w-[300px]">
+        <AccessLinks
+          icon={<FaGithub />}
+          url="https://github.com/k4nkan"
+          title="Github"
+        />
+        <AccessLinks
+          icon={<FaFacebook />}
+          url="https://www.facebook.com/profile.php?id=61558039312920"
+          title="Facebook"
+        />
+        <AccessLinks
+          icon={<FaInstagramSquare />}
+          url="https://www.instagram.com/yamakan126?igsh=eDh6cWRuYmxxaWk4&utm_source=qr"
+          title="Instagram"
+        />
+        <AccessLinks
+          icon={<FaTwitterSquare />}
+          url="https://x.com/caaantaaa?s=21"
+          title="X (Twitter)"
+        />
+      </div>
     </div>
   );
 };
