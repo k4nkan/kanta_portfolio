84db3e428875eb6d307170584ac8d59c732bdc09
k4nkan <kanta.y.0126@gmail.com>
2025-01-22 03:35:23 +0900

    #30 不要なインポートの消去

--- my-app/src/pages/access/index.tsx
+++ my-app/src/pages/access/index.tsx
@@ -1,4 +1,4 @@
-import { color, motion } from "framer-motion";
+import {motion } from "framer-motion";
 import AccessLinks from "../../components/AccessLinks";
 import { FaGithub } from "react-icons/fa";
 import { FaFacebook } from "react-icons/fa";
