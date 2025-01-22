76bac1a602cf4badca76b8e977dcd86e22975b65
k4nkan <kanta.y.0126@gmail.com>
2025-01-23 01:59:35 +0900

    #52 framer-motion exit動かないバグの修正

--- my-app/src/pages/_app.tsx
+++ my-app/src/pages/_app.tsx
@@ -1,14 +1,11 @@
 import Header from "../components/HeaderComponent/Header";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import useMedia from "use-media";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  const isDesktop = useMedia({ minWidth: "640px" });
-
   return (
     <div className="min-w-[320px]">
-      {isDesktop ? <Header /> : <Header />}
+      <Header />
       <Component {...pageProps} />
     </div>
   );
