90ea184617e9320f834b896eeb12289780a16fcf
k4nkan <kanta.y.0126@gmail.com>
2025-01-26 03:22:38 +0900

    #60 エラーページの修正

--- my-app/src/pages/404.tsx
+++ my-app/src/pages/404.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React from "react";
 
 const Custom404 = () => {
   return (
-    <>
+    <div
+      className="flex flex-col text-2xl gap-2 justify-center items-center pt-[65px]"
+      style={{ minHeight: "calc(100vh - 65px)" }}
+    >
       <h1>404 - not found</h1>
       <p>申し訳ございません</p>
       <p>ページが見つかりません...</p>
-    </>
+    </div>
   );
 };
 
