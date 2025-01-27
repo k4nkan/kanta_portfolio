1115c6ccfd17225ee72d96a53ae1fe29f24de8ce
k4nkan <kanta.y.0126@gmail.com>
2025-01-27 17:11:39 +0900

    #66 画像周りの調整

--- my-app/src/pages/index.tsx
+++ my-app/src/pages/index.tsx
@@ -1,23 +1,18 @@
 import React, { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 
-const images = [
-  "profile_images/profile_1.jpeg",
-  "profile_images/profile_2.jpeg",
-  "profile_images/profile_3.jpeg",
-  "profile_images/profile_4.jpeg",
-];
+const images = ["/profile_images/top.jpeg"];
 
 export default function Home() {
-  const [currentIndex, setCurrentIndex] = useState(0);
+  const [imageIndex, setImageIndex] = useState(0);
 
   const changeImage = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    setImageIndex((prevIndex) => (prevIndex + 1) % images.length);
   };
 
   return (
     <div
-      className="flex justify-center items-center bg-white bg-opacity-50"
+      className="flex justify-center items-center bg-white bg-opacity-60"
       style={{
         minHeight: "100vh",
         width: "100%",
@@ -27,8 +22,8 @@ export default function Home() {
     >
       <AnimatePresence>
         <motion.img
-          key={currentIndex}
-          src={images[currentIndex]}
+          key={imageIndex}
+          src={images[imageIndex]}
           alt="Portfolio"
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
@@ -36,19 +31,16 @@ export default function Home() {
           transition={{ duration: 0.5 }}
           style={{
             position: "absolute",
-            top: 0,
-            left: 0,
-            width: "100%",
             height: "100%",
+            width: "100%",
             objectFit: "cover",
             zIndex: -1,
           }}
         />
       </AnimatePresence>
 
-      {/* テキスト */}
       <div
-        className="text-center text-4xl text-black"
+        className="text-center text-4xl md:text-6xl text-black"
         style={{
           position: "relative",
           zIndex: 1,
